d8cfb10e845f706fd018b59c2ac8b7eed3679c82
Hayden <48267247+hayden-fr@users.noreply.github.com>
2025-01-16 16:39:11 +0800

    feat: Browse images in the same folder with left and right direction keys (#38)
    * feat: Use left/right direction keys to navigate preview
    
    * feat: Use esc key to close preview window
    
    * fix: Remove invalid code
    
    * feat(preview): Add navigate operate button
    
    * chore: Update README

--- src/components/GlobalPreview.vue
+++ src/components/GlobalPreview.vue
@@ -2,9 +2,8 @@
   <div
     v-show="visible"
     class="fixed left-0 top-0 z-[5000] flex h-full w-full items-center justify-center bg-black/40 dark:bg-black/60"
-    @click="close"
   >
-    <div class="absolute right-3 top-3" @click.stop="">
+    <div class="absolute right-3 top-3">
       <Button
         icon="pi pi-times"
         severity="secondary"
@@ -12,12 +11,28 @@
         @click="close"
       ></Button>
     </div>
-    <div class="h-full w-full select-none p-10" @click.stop="">
+    <div class="h-full w-full select-none p-10">
       <img
         class="h-full w-full object-contain"
         :src="`/image-browsing${current?.fullname}`"
         alt="preview"
       />
+    </div>
+    <div class="absolute left-2 top-1/2">
+      <Button
+        icon="pi pi-angle-left"
+        severity="secondary"
+        rounded
+        @click="openPreviousImage"
+      ></Button>
+    </div>
+    <div class="absolute right-2 top-1/2">
+      <Button
+        icon="pi pi-angle-right"
+        severity="secondary"
+        rounded
+        @click="openNextImage"
+      ></Button>
     </div>
   </div>
 </template>
@@ -26,5 +41,6 @@
 import { usePreview } from 'hooks/preview'
 import Button from 'primevue/button'
 
-const { visible, current, close } = usePreview()
+const { visible, current, close, openPreviousImage, openNextImage } =
+  usePreview()
 </script>
--- src/hooks/preview.ts
+++ src/hooks/preview.ts
@@ -1,17 +1,57 @@
 import { defineStore } from 'hooks/store'
 import { DirectoryItem } from 'types/typings'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
-export const usePreview = defineStore('preview', () => {
+export const usePreview = defineStore('preview', (store) => {
   const visible = ref(false)
   const current = ref<DirectoryItem>()
+  const currentIndex = ref(0)
+
+  const imageItems = computed(() => {
+    return store.explorer.items.value.filter((item) => {
+      return item.type === 'image'
+    })
+  })
+
+  const openPreviousImage = () => {
+    currentIndex.value--
+    if (currentIndex.value < 0) {
+      currentIndex.value = imageItems.value.length - 1
+    }
+    const item = imageItems.value[currentIndex.value]
+    current.value = item
+  }
+
+  const openNextImage = () => {
+    currentIndex.value++
+    if (currentIndex.value > imageItems.value.length - 1) {
+      currentIndex.value = 0
+    }
+    const item = imageItems.value[currentIndex.value]
+    current.value = item
+  }
+
+  const previewKeyboardListener = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      close()
+    }
+    if (event.key === 'ArrowLeft') {
+      openPreviousImage()
+    }
+    if (event.key === 'ArrowRight') {
+      openNextImage()
+    }
+  }
 
   const open = (item: DirectoryItem) => {
     visible.value = true
     current.value = item
+    currentIndex.value = imageItems.value.indexOf(item)
+    document.addEventListener('keyup', previewKeyboardListener)
   }
 
   const close = () => {
+    document.removeEventListener('keyup', previewKeyboardListener)
     visible.value = false
   }
 
@@ -20,6 +60,8 @@ export const usePreview = defineStore('preview', () => {
     current,
     open,
     close,
+    openPreviousImage,
+    openNextImage,
   }
 })
 
