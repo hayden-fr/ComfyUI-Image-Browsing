944e2a90884b5e010ffabc42ffc1206ea637eee0
hayden <48267247+hayden-fr@users.noreply.github.com>
2025-08-12 16:12:23 +0800

    chore(i18n): refactor i18n code

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,53 +1,12 @@
 import { app } from 'scripts/comfyAPI'
 import { createI18n } from 'vue-i18n'
 
+import en from './locales/en.json'
+import zh from './locales/zh.json'
+
 const messages = {
-  en: {
-    outputExplorer: 'Output Explorer',
-    openOutputExplorer: 'Open Output Explorer',
-    searchInFolder: 'Search in {0}',
-    open: 'View',
-    openInNewTab: 'View in new tab',
-    save: 'Save',
-    rename: 'Rename',
-    download: 'Archive & Download',
-    delete: 'Delete',
-    deleteAsk: 'Confirm delete {0}?',
-    selectedItems: 'Selected Items',
-    addFolder: 'Add folder',
-    uploadFile: 'Upload file',
-    newFolderName: 'New Folder',
-    selected: 'Selected',
-    items: 'Items',
-    cancel: 'Cancel',
-    confirm: 'Confirm',
-    setting: {
-      showDeleteConfirm: 'Show delete confirmation dialog',
-    },
-  },
-  zh: {
-    outputExplorer: '输出浏览器',
-    openOutputExplorer: '打开输出浏览器',
-    searchInFolder: '在 {0} 中搜索',
-    open: '打开',
-    openInNewTab: '在新标签页中打开',
-    save: '保存',
-    rename: '重命名',
-    download: '打包并下载',
-    delete: '删除',
-    deleteAsk: '确认删除{0}?',
-    selectedItems: '已选项目',
-    addFolder: '新增文件夹',
-    uploadFile: '上传文件',
-    newFolderName: '新建文件夹',
-    selected: '已选',
-    items: '个项目',
-    cancel: '取消',
-    confirm: '确认',
-    setting: {
-      showDeleteConfirm: '显示删除确认对话框',
-    },
-  },
+  en: en,
+  zh: zh,
 }
 
 const getLocalLanguage = () => {
